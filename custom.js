eb9cb05327511494ff5dd20ba709ac937ef31552
asheralley <asher.asheralley@gmail.com>
2020-06-09 15:24:47 +1200

    added sticky nav functionality

--- custom.js
+++ custom.js
@@ -1,1 +1,24 @@
 // Custom JavaScript -> Asher Alley
+
+// sticky nav on scoll
+function stickyNavOnScroll () {
+    window.onscroll = function() {myFunction()};
+
+    // Get the header
+    let header = document.getElementById("stickyNav");
+
+    // Get the offset position of the navbar
+    let sticky = header.offsetTop;
+
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+}
+
+// call the sticky nav onscroll function
+stickyNavOnScroll();
