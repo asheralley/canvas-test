655bd9ce2b6cd7438f0b1145550ae9bdc2f2fdc7
asheralley <asher.asheralley@gmail.com>
2021-03-06 11:55:15 +1300

    removed merge conflict tags in custom.js

--- custom.js
+++ custom.js
@@ -10,8 +10,8 @@ function stickyNavOnScroll() {
   };
 
   // Get the header
-  let header = document.getElementById('stickyNav');
-  let about = document.getElementById('about');
+  let header = document.getElementById("stickyNav");
+  let about = document.getElementById("about");
 
   // Get the offset position of the navbar
   let sticky = header.offsetTop;
@@ -19,11 +19,11 @@ function stickyNavOnScroll() {
   // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
   function addRemoveClass() {
     if (window.pageYOffset > sticky) {
-      header.classList.add('sticky');
-      about.classList.add('nav-offset');
+      header.classList.add("sticky");
+      about.classList.add("nav-offset");
     } else {
-      header.classList.remove('sticky');
-      about.classList.remove('nav-offset');
+      header.classList.remove("sticky");
+      about.classList.remove("nav-offset");
     }
   }
 }
@@ -32,38 +32,38 @@ function stickyNavOnScroll() {
 stickyNavOnScroll();
 
 function formSubmit(event) {
-  let url = 'send_message.php';
+  let url = "send_message.php";
   let request = new XMLHttpRequest();
-  let feedBack = document.getElementById('feedBackMsg');
-  let nameInput = document.getElementById('name');
-  let emailInput = document.getElementById('email');
-  let messageInput = document.getElementById('message');
-  let service = document.getElementById('serviceCheck');
+  let feedBack = document.getElementById("feedBackMsg");
+  let nameInput = document.getElementById("name");
+  let emailInput = document.getElementById("email");
+  let messageInput = document.getElementById("message");
+  let service = document.getElementById("serviceCheck");
 
   if (service.checked) {
-    feedBack.innerHTML = 'Thanks for getting in touch!';
+    feedBack.innerHTML = "Thanks for getting in touch!";
     event.preventDefault();
     return;
   }
 
   // hide send message button
-  let submitBtn = document.getElementById('submitBtn');
-  submitBtn.style.display = 'none';
-  feedBack.innerHTML = 'Sending message...';
+  let submitBtn = document.getElementById("submitBtn");
+  submitBtn.style.display = "none";
+  feedBack.innerHTML = "Sending message...";
 
-  request.open('POST', url, true);
+  request.open("POST", url, true);
   request.onload = function () {
     // request successful
     // we can use server response to our request now
     // handle successful send message here
     if (request.status == 200) {
-      let messageText = 'Message sent! Thanks for getting in touch.';
+      let messageText = "Message sent! Thanks for getting in touch.";
       feedBack.innerHTML = messageText;
-      nameInput.value = '';
-      emailInput.value = '';
-      messageInput.value = '';
+      nameInput.value = "";
+      emailInput.value = "";
+      messageInput.value = "";
     } else {
-      let messageText = 'Whoops! Something went wrong, please try again later.';
+      let messageText = "Whoops! Something went wrong, please try again later.";
       feedBack.innerHTML = messageText;
     }
   };
@@ -71,7 +71,7 @@ function formSubmit(event) {
   request.onerror = function () {
     // request failed
     // handle errors here
-    let messageText = 'Whoops! Something went wrong, please try again later.';
+    let messageText = "Whoops! Something went wrong, please try again later.";
     feedBack.innerHTML = messageText;
   };
 
@@ -81,13 +81,12 @@ function formSubmit(event) {
 
 // and you can attach form submit event like this for example
 function attachFormSubmitEvent(formId) {
-  document.getElementById(formId).addEventListener('submit', formSubmit);
+  document.getElementById(formId).addEventListener("submit", formSubmit);
 }
 
-attachFormSubmitEvent('contactForm');
+attachFormSubmitEvent("contactForm");
 
 // smooth scroll to elements
-<<<<<<< HEAD
 //function smoothScroll(e, offSetVal) {
 //  let el = e.target;
 //  let parent = el.parentElement;
@@ -108,25 +107,24 @@ attachFormSubmitEvent('contactForm');
 //document.getElementById("mainNav").addEventListener("click", function (e) {
 //  smoothScroll(e, -80);
 //});
-=======
+
 function smoothScroll(e, offSetVal) {
   let el = e.target;
   let parent = el.parentElement;
-  let anchor = parent.getAttribute('loc');
+  let anchor = parent.getAttribute("loc");
   let yOffset = offSetVal;
   //document.getElementById(anchor).scrollIntoView({ behavior: "smooth" });
   let element = document.getElementById(anchor);
-  if (anchor === 'contact') {
+  if (anchor === "contact") {
     yOffset = 0;
   }
   let y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-  window.scrollTo({ top: y, behavior: 'smooth' });
+  window.scrollTo({ top: y, behavior: "smooth" });
 }
 
-document.getElementById('stickyNav').addEventListener('click', function (e) {
+document.getElementById("stickyNav").addEventListener("click", function (e) {
   smoothScroll(e, 0);
 });
-document.getElementById('mainNav').addEventListener('click', function (e) {
+document.getElementById("mainNav").addEventListener("click", function (e) {
   smoothScroll(e, -80);
 });
->>>>>>> 209b6417a5ed6fb48fb266098c566711e95379bb
